b760081a0ad58746308b3b80f67befe74f29860d
Michal Radomski <michalra@op.pl>
2024-01-31 00:27:06 +0100

    GraphQL with React course - commit 25

--- 02 GraphQL_React/02/client/src/components/SongCreate.tsx
+++ 02 GraphQL_React/02/client/src/components/SongCreate.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import gql from "graphql-tag";
+
+const mutation = gql`
+  mutation {
+    addSong(title: "Hot Summer Night")
+  }
+`;
 
 class SongCreate extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -6,11 +13,16 @@ class SongCreate extends React.Component<Props, State> {
     this.state = { title: "" };
   }
 
+  onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   render() {
+    // console.log("this.state:", this.state);
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
           <input onChange={(event) => this.setState({ title: event.target.value })} value={this.state.title} />
         </form>
