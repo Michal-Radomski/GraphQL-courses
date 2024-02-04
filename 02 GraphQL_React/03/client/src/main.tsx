76ea13f578f86586f7ee0d5738534d632981ce3b
Michal Radomski <michalra@op.pl>
2024-02-04 11:10:39 +0100

    GraphQL with React course - commit 49

--- 02 GraphQL_React/03/client/src/main.tsx
+++ 02 GraphQL_React/03/client/src/main.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { Router, Route, hashHistory } from "react-router";
 
 import "./App.scss";
 import App from "./App";
@@ -8,10 +13,21 @@ if (module.hot) {
   module.hot.accept();
 }
 
+const apolloClient = new ApolloClient({
+  link: new HttpLink({ uri: "/graphql" }),
+  cache: new InMemoryCache({
+    dataIdFromObject: (o) => o.id,
+  }),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   //* React.StrictMode blocks apolloClient! */}
   <React.Fragment>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}></Route>
+      </Router>
+    </ApolloProvider>
   </React.Fragment>
 );
