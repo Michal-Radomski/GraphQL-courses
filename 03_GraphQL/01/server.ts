0b81be096fde4edc4592d5fa47def765c5778937
Michal Radomski <michalra@op.pl>
2024-03-16 12:30:39 +0100

    GraphQL courese - commit 01

---
+++ 03_GraphQL/01/server.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+(async function () {
+  const { url } = await startStandaloneServer(server, { listen: { port: 9000 } });
+  console.log(`Server running at ${url}`);
+})();
